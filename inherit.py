918b16895881907fcd8e556691cc4721aa49469d
sushma1810 <39873482+sushma1810@users.noreply.github.com>
2018-06-04 10:18:01 +0530

    Update inherit.py

--- inherit.py
+++ inherit.py
@@ -1,19 +1,19 @@
-class sanjay:
+class sushma:
 	something=123
 	def __init__(self):
 		print("this is class sanjay")
 	def set(self,anything):
 		sanjay.something=anything
 	def get(self):
-		print("something=:",sanjay.something)
-class manaswini(sanjay):
+		print("something=:",sushma.something)
+class manaswini(sushma):
 	def __init__(self):
 		print("manaswini")
 	def get(self):
-		print("anything of manaswini:",sanjay.something)
+		print("anything of manaswini:",sushma.something)
 s2=manaswini()
 s2.set(234)
-s1=sanjay()
+s1=sushma()
 s1.get()
 s2.get()
 
